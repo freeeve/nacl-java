c17479ae65ed0b33c4e66729b9115f609f1217d7
Wes Freeman <wes@skeweredrook.com>
2015-02-17 15:48:38 -0500

    curve25519 works!

--- src/test/java/com/caligollc/nacl/Curve25519Test.java
+++ src/test/java/com/caligollc/nacl/Curve25519Test.java
@@ -18,12 +18,22 @@ public class Curve25519Test {
 		byte out[] = b;
 		a[0] = 1;
 
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 200; i++) {
 			out = Curve25519.scalarBaseMult(in);
 			byte temp[] = in;
 			in = out;
 			out = temp;
 		}
 		assertEquals(expectedHex, DatatypeConverter.printHexBinary(in).toLowerCase());
+	}
+
+	@Test
+	public void TestFeSquareFail() {
+		long startarr[] = {12562720, 6296353, 54578601, 16746890, -54591682, -18847443, 46849294, 26819245, -35365487, -3788969};
+		Curve25519.FieldElement start = new Curve25519.FieldElement(startarr);
+		long expectedarr[] = {19358458, -14374687, 30832724, 1573657, 30534596, 6957402, 18251160, 11463428, -8323465, -4774430};
+		Curve25519.FieldElement expected = new Curve25519.FieldElement(expectedarr);
+		Curve25519.FieldElement square = start.square();
+		assertEquals(expected, square);
 	}
 }
